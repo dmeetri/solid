9b08cab556f394559778f04e0b704b352618944a
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-09-04 18:31:37 +0900

    add tiket model

--- solid/tickets/models.py
+++ solid/tickets/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class TicketModel(models.Model):
+    otdel = models.CharField(
+        max_length=100
+    )
+    desceiption = models.TextField()
+    ticket_number = models.CharField(
+        max_length=20,
+        unique=True,
+        editable=False
+    )
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
+
+    class Status(models.TextChoices):
+        NEW = 'new', 'Ожидает принятия'
+        ACCEPTED = 'accepted', 'Принята'
+        IN_PROGRESS = 'in_progress', 'Выполняется'
+        COMPLETED = 'completed', 'Завершена'
+        REJECTED = 'rejected', 'Отклонена'
+
+    status = models.CharField(
+        choices=Status.choices,
+        default=Status.NEW,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    accepted_at = models.DateTimeField(null=True, blank=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.ticket_number:
+            last = TicketModel.objects.all().order_by('id').last()
+
+            if last:
+                num = int(last.ticket_number[-6:]) + 1
+            else:
+                num = 1
+
+            self.ticket_number = f"{timezone.now().year}{num:06d}"
+        super().save(*args, **kwargs)
