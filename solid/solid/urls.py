8329495aa0122c1d5122efd43e2ff1f29c60f46a
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-09-06 15:04:23 +0900

    add header

--- solid/solid/urls.py
+++ solid/solid/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('auth', include('users.urls')),
+    path('auth/', include('users.urls')),
     path('', include('tickets.urls')),
 ]
